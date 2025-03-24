bd8ad473b4136d3ab8c69377504bfbb441744590
CdReyes460 <creyes@filamer.edu.ph>
2025-03-24 00:20:06 -0700

    Function activity 1

---
+++ Activities/Function - activitites/script.js
@@ -0,0 +1,13 @@
+function whatShallwear (temp) {
+    if (temp <60) {
+        console.log ("wear a jacket");  
+    } else if (temp < 70) {
+        console.log("wear a sweater");
+    } else {
+        console.log("wear a t-shirt");
+    }
+}
+
+whatShallwear(50);
+whatShallwear(80);
+whatShallwear(60);
